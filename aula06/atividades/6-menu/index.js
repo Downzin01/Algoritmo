dbf35320b89fb131ce90cabd95ec6b46c7959d1b
Downzin01 <downmestre@gmail.com>
2024-08-19 02:08:48 -0300

    Pasta da atividade 6

---
+++ aula06/atividades/6-menu/index.js
@@ -0,0 +1,26 @@
+function gerarBotoes() {
+    const container = document.getElementById('botoesContainer');
+    const paginas = [
+        { src: "tabuada" },
+        { src: "somarNumeroEntre" },
+        { src: "somarNumeroUsuario" },
+        { src: "sequenciaFibonacci" },
+        { src: "inverterString" },
+        { src: "thanks" }
+    ];
+    for (let i = 1; i <= 6; i++) {
+        const botao = document.createElement('button');
+        botao.className = 'card-menu-botao';
+        botao.textContent = i;
+
+        // Define o comportamento ao clicar no botão
+        botao.onclick = function() {
+            const novaUrl = window.location.href.replace(/6-menu\/.*/, '') + `${i}-${paginas[i - 1].src}/index.html`
+            window.location.href = novaUrl;
+        };
+
+        container.appendChild(botao);
+    }
+}
+
+gerarBotoes();
